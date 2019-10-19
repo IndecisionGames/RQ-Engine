54cf9a02602edbf5f5c5365798b377c886329898
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2019-10-19 14:00:44 +0100

    Create CMakefile

---
+++ libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Clock.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Err.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Err.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/FileInputStream.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/FileInputStream.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Lock.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Lock.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/MemoryInputStream.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/MemoryInputStream.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Mutex.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Mutex.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Sleep.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Sleep.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/String.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/String.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Thread.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Thread.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/ThreadLocal.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/ThreadLocal.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Time.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Win32/ClockImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/ClockImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Win32/MutexImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/MutexImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Win32/SleepImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/SleepImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Win32/ThreadImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/ThreadImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/System/Win32/ThreadLocalImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/Win32/ThreadLocalImpl.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_SYSTEM_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Clipboard.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Clipboard.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Context.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Context.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Cursor.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Cursor.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/EGLCheck.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/EGLCheck.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/EglContext.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/EglContext.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/GlContext.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/GlContext.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/GlResource.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/GlResource.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Joystick.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Joystick.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/JoystickManager.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/JoystickManager.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Keyboard.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Keyboard.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Mouse.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Mouse.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Sensor.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Sensor.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/SensorManager.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/SensorManager.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Touch.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Touch.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/VideoMode.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/VideoMode.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Vulkan.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Vulkan.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/ClipboardImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/ClipboardImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/CursorImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/CursorImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/InputImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/InputImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/JoystickImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/JoystickImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/SensorImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/SensorImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/VideoModeImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/VideoModeImpl.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/VulkanImplWin32.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/VulkanImplWin32.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/WglContext.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WglContext.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Win32/WindowImplWin32.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Win32/WindowImplWin32.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/Window.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/Window.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/WindowBase.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/WindowBase.cpp.obj"
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/src/SFML/Window/WindowImpl.cpp" "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/Window/CMakeFiles/sfml-window.dir/WindowImpl.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SFML_WINDOW_EXPORTS"
+  "UNICODE"
+  "_UNICODE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../include"
+  "../../src"
+  "../../extlibs/headers/glad/include"
+  "../../extlibs/headers/vulkan"
+  "../../extlibs/headers/mingw"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "E:/Workspace/IndecisionGames/Projects/RQ-Engine/libs/SFML/cmake/SFML/src/SFML/System/CMakeFiles/sfml-system.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
