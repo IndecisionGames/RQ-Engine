9b083f08b8ee911cc4710cdc01a01b698bb7af7b
Sivarjuen Ravichandran <sivarjuen.ravichandran@hotmail.com>
2019-10-19 18:09:51 +0100

    Modified build script to automatically copy DLLs for Windows systems

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]] 
+then
+	WINDOWS=true
+else
+	WINDOWS=false
+fi
+
 mkdir -p bin
 cd bin
-cmake ..
 
-if [[ "$OSTYPE" == "win32" ]]; then
+if [[ "$WINDOWS" ]]; then
+	cmake .. -G "MinGW Makefiles"
     mingw32-make
+	SOURCE='libs/SFML/lib'
+	for file in $SOURCE/*.dll
+	do
+		cp ${file} .
+	done
 else
+	cmake ..
     make
 fi
+cd ..
+$SHELL
