8e5f9b4f32e76e96befc211c1c856829daa5f401
Luke Quinlan <lukeq1000@gmail.com>
2019-10-24 23:17:12 +0100

    fix RM on mac, get functions for font and sound

--- src/core/ResourceManager.cpp
+++ src/core/ResourceManager.cpp
@@ -13,57 +13,75 @@ ResourceManager::ResourceManager() {}
 ResourceManager::~ResourceManager() {}
 
 void ResourceManager::cleanUp() {
-	textureMap.clear();
+    textureMap.clear();
 }
 
 bool ResourceManager::loadTexture(const std::string &filepath, const std::string &textureName, bool smooth) {
-	sf::Texture newTexture;
-	if (!newTexture.loadFromFile(filepath))
-	{
-		printf("Unable to load texture from %s!\n", filepath.c_str());
-		return false;
-	}
-	newTexture.setSmooth(smooth);
-	textureMap.insert_or_assign(textureName, newTexture);
-	return true;
+    sf::Texture newTexture;
+    if (!newTexture.loadFromFile(filepath))
+    {
+        printf("Unable to load texture from %s!\n", filepath.c_str());
+        return false;
+    }
+    newTexture.setSmooth(smooth);
+    textureMap[textureName] = newTexture;
+    return true;
 }
 
 bool ResourceManager::loadSound(const std::string& filepath, const std::string& soundName) {
-	sf::SoundBuffer newSoundBuffer;
-	if (!newSoundBuffer.loadFromFile(filepath))
-	{
-		printf("Unable to load sound from %s!\n", filepath.c_str());
-		return false;
-	}
-	soundMap.insert_or_assign(soundName, newSoundBuffer);
-	return true;
+    sf::SoundBuffer newSoundBuffer;
+    if (!newSoundBuffer.loadFromFile(filepath))
+    {
+        printf("Unable to load sound from %s!\n", filepath.c_str());
+        return false;
+    }
+    soundMap[soundName] = newSoundBuffer;
+    return true;
 }
 
 bool ResourceManager::loadFont(const std::string& filepath, const std::string& fontName) {
-	sf::Font newFont;
-	if (!newFont.loadFromFile(filepath))
-	{
-		printf("Unable to load font from %s!\n", filepath.c_str());
-		return false;
-	}
-	fontMap.insert_or_assign(fontName, newFont);
-	return true;
+    sf::Font newFont;
+    if (!newFont.loadFromFile(filepath))
+    {
+        printf("Unable to load font from %s!\n", filepath.c_str());
+        return false;
+    }
+    fontMap[fontName] = newFont;
+    return true;
 }
 
 // TODO: Refactor to sprite's wrapper class (entity)
 sf::Sprite ResourceManager::createSprite(const std::string &textureName,
-	int originX, int originY, int width, int height) {
-	sf::Sprite newSprite;
+    int originX, int originY, int width, int height) {
+    sf::Sprite newSprite;
 
-	auto textureIt = textureMap.find(textureName);
-	if (textureIt == textureMap.end()) {
-		printf("Unable to load texture %s!\n", textureName.c_str());
-	}
-	else {
-		newSprite.setTexture(textureIt->second);
-		if (originX != -1 && originY != -1 && width != -1 && height != -1) {
-			newSprite.setTextureRect(sf::IntRect(originX, originY, width, height));
-		}
-	}
-	return newSprite;
+    auto textureIt = textureMap.find(textureName);
+    if (textureIt == textureMap.end()) {
+        printf("Unable to load texture %s!\n", textureName.c_str());
+    }else{
+        newSprite.setTexture(textureIt->second);
+        newSprite.setTextureRect(sf::IntRect(originX, originY, width, height));
+    }
+    return newSprite;
+}
+
+sf::Font* ResourceManager::getFont(const std::string &fontName){
+    auto fontIt = fontMap.find(fontName);
+    if (fontIt == fontMap.end()) {
+        printf("Unable to load font %s!\n", fontName.c_str());
+        return NULL;
+    }else{
+        return &fontIt->second;
+    }
+    return NULL;
+}
+
+sf::SoundBuffer* ResourceManager::getSound(const std::string &soundName){
+    auto soundIt = soundMap.find(soundName);
+    if (soundIt == soundMap.end()) {
+        printf("Unable to load sound %s!\n", soundName.c_str());
+        return NULL;
+    }else{
+        return &soundIt->second;
+    }
 }
--- src/core/ResourceManager.hpp
+++ src/core/ResourceManager.hpp
@@ -4,33 +4,32 @@
 
 namespace RQEngine {
 
-	class ResourceManager {
+    class ResourceManager {
 
-	public:
-		ResourceManager();
-		~ResourceManager();
+    public:
+        ResourceManager();
+        ~ResourceManager();
 
-		static void cleanUp();
+        static void cleanUp();
 
-		bool loadTexture(const std::string &filepath, const std::string &textureName, bool smooth = false);
+        bool loadTexture(const std::string &filepath, const std::string &textureName, bool smooth = false);
 
-		bool loadSound(const std::string& filepath, const std::string& soundName);
+        bool loadSound(const std::string& filepath, const std::string& soundName);
 
-		bool loadFont(const std::string& filepath, const std::string& fontName);
+        bool loadFont(const std::string& filepath, const std::string& fontName);
 
-		// TODO: Refactor to sprite's wrapper class
-		static sf::Sprite createSprite(const std::string &textureName,
-			int originX = -1, int originY = -1, int width = -1, int height = -1);
+        // TODO: Refactor to sprite's wrapper class
+        static sf::Sprite createSprite(const std::string &textureName,
+            int originX = 0, int originY = 0, int width = 0, int height = 0);
 
-	private:
-		static std::unordered_map<std::string, sf::Texture> textureMap;
-		static std::unordered_map<std::string, sf::SoundBuffer> soundMap;
-		static std::unordered_map<std::string, sf::Font> fontMap;
-	};
+        static sf::Font* getFont(const std::string &fontName);
+        static sf::SoundBuffer* getSound(const std::string &soundName);
 
+    private:
+        static std::unordered_map<std::string, sf::Texture> textureMap;
+        static std::unordered_map<std::string, sf::SoundBuffer> soundMap;
+        static std::unordered_map<std::string, sf::Font> fontMap;
 
-
-
-
+    };
 
 }
